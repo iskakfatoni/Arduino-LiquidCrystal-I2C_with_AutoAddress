a311802f4b2bed4dde8cb693c4b78af66a3d9820
Iskak Fatoni <iskakfatoni@gmail.com>
2025-02-18 14:22:34 +0700

    re-introduce init function

--- examples/HelloWorld_oldCode/HelloWorld_oldCode.ino
+++ examples/HelloWorld_oldCode/HelloWorld_oldCode.ino
@@ -1,13 +1,13 @@
-// HELLO WORLD NEW using autoBegin function
+#include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-// Set the LCD for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(16, 2);
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
 	// initialize the LCD
-	lcd.autoBegin();
+	lcd.init();
 
 	// Turn on the blacklight and print a message.
 	lcd.backlight();
