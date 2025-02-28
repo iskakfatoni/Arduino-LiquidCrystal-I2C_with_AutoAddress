2a6f7ad66b2fcd7473128877c7ba4bc4851ecae6
Iskak Fatoni <iskakfatoni@gmail.com>
2025-02-28 08:50:52 +0700

    Update LiquidCrystal_I2C.cpp

--- LiquidCrystal_I2C.cpp
+++ LiquidCrystal_I2C.cpp
@@ -36,7 +36,6 @@ Wire.begin();
 byte error, address;
 int nDevices;
 Serial.println("Scanning...");
-     
       nDevices = 0;
       for(address = 39; address <= 63; address++ )
       {
@@ -69,82 +68,13 @@ Serial.println("Scanning...");
 
 }
 
-
-/*
 void LiquidCrystal_I2C::begin() {
-	Wire.begin();
-	_displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
-
-	if (_rows > 1) {
-		_displayfunction |= LCD_2LINE;
-	}
-
-	// for some 1 line displays you can select a 10 pixel high font
-	if ((_charsize != 0) && (_rows == 1)) {
-		_displayfunction |= LCD_5x10DOTS;
-	}
-
-	// SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
-	// according to datasheet, we need at least 40ms after power rises above 2.7V
-	// before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
-	delay(50); 
-
-	// Now we pull both RS and R/W low to begin commands
-	expanderWrite(_backlightval);	// reset expanderand turn backlight off (Bit 8 =1)
-	delay(1000);
-
-	//put the LCD into 4 bit mode
-	// this is according to the hitachi HD44780 datasheet
-	// figure 24, pg 46
-
-	// we start in 8bit mode, try to set 4 bit mode
-	write4bits(0x03 << 4);
-	delayMicroseconds(4500); // wait min 4.1ms
-
-	// second try
-	write4bits(0x03 << 4);
-	delayMicroseconds(4500); // wait min 4.1ms
-
-	// third go!
-	write4bits(0x03 << 4); 
-	delayMicroseconds(150);
-
-	// finally, set to 4-bit interface
-	write4bits(0x02 << 4); 
-
-	// set # lines, font size, etc.
-	command(LCD_FUNCTIONSET | _displayfunction);  
-	
-	// turn the display on with no cursor or blinking default
-	_displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
-	display();
-	
-	// clear it off
-	clear();
-	
-	// Initialize to default text direction (for roman languages)
-	_displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
-	
-	// set the entry mode
-	command(LCD_ENTRYMODESET | _displaymode);
-	
-	home();
-	print("@iskakfatoni");
-	setCursor(0,1);
-	print("alamat lcd: 0x");
-	print(_addr,HEX);
-	delay(2500);
-	clear(); home();
-}   */
-
-void LiquidCrystal_I2C::begin() {
-		//AUTOADDRESS
+	//AUTOADDRESS
 	Wire.begin();
 	byte error, address;
 	int nDevices;
 	Serial.println("Scanning...");
-     
-      	nDevices = 0;
+       	nDevices = 0;
       	for(address = 39; address <= 63; address++ )
       	{
         	Wire.beginTransmission(address);
@@ -236,72 +166,10 @@ void LiquidCrystal_I2C::begin() {
 	setCursor(0,1);
 	print("alamat lcd: 0x");
 	print(_addr,HEX);
-	delay(2500);
+	delay(1500);
 	clear();
 	home();
 }
-
-
-/*
-void LiquidCrystal_I2C::begin2() {
-	Wire.begin();
-	_displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
-
-	if (_rows > 1) {
-		_displayfunction |= LCD_2LINE;
-	}
-
-	// for some 1 line displays you can select a 10 pixel high font
-	if ((_charsize != 0) && (_rows == 1)) {
-		_displayfunction |= LCD_5x10DOTS;
-	}
-
-	// SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
-	// according to datasheet, we need at least 40ms after power rises above 2.7V
-	// before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
-	delay(50); 
-
-	// Now we pull both RS and R/W low to begin commands
-	expanderWrite(_backlightval);	// reset expanderand turn backlight off (Bit 8 =1)
-	delay(1000);
-
-	//put the LCD into 4 bit mode
-	// this is according to the hitachi HD44780 datasheet
-	// figure 24, pg 46
-
-	// we start in 8bit mode, try to set 4 bit mode
-	write4bits(0x03 << 4);
-	delayMicroseconds(4500); // wait min 4.1ms
-
-	// second try
-	write4bits(0x03 << 4);
-	delayMicroseconds(4500); // wait min 4.1ms
-
-	// third go!
-	write4bits(0x03 << 4); 
-	delayMicroseconds(150);
-
-	// finally, set to 4-bit interface
-	write4bits(0x02 << 4); 
-
-	// set # lines, font size, etc.
-	command(LCD_FUNCTIONSET | _displayfunction);  
-	
-	// turn the display on with no cursor or blinking default
-	_displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
-	display();
-	
-	// clear it off
-	clear();
-	
-	// Initialize to default text direction (for roman languages)
-	_displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
-	
-	// set the entry mode
-	command(LCD_ENTRYMODESET | _displaymode);
-	
-	home();
-}  */
 
 void LiquidCrystal_I2C::autoBegin() {
 	//AUTOADDRESS
